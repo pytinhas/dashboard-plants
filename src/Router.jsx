d23717c476bc1a08b9f92ba64691bf562db8f8bd
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-09-23 12:04:44 -0300

    Add files via upload

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contato } from './pages/Contato';
 import { Off } from "./pages/Off";
 import { On } from "./pages/On";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Off />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Contato" element={<Contato />} />      
       <Route path="/Off" element={<Off />} />      
       <Route path="/On" element={<On />} />           
     </Routes>
